c83742c2ceb6bcc5468d0a71d982590e959a376f
dgbragas <dgbragsa.dev@gmail.com>
2021-02-15 15:56:13 -0300

    feat: connecting server to mondodb

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const routes = require('./routes');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 const app = express();
 app.use(express.json());
