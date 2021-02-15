c220b871b3db805792bcaf976b86343488867921
dgbragas <dgbragas.dev@gmail.com>
2021-02-15 19:31:44 -0300

    feat: importing <Orders /> and logo in <App />

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,17 @@
 import GlobalStyle from './styles/global';
+import { Container } from './styles';
+
+import Orders from './components/Orders';
+
+import logoImg from './images/ezOrders-logo.svg';
 
 const App = () => (
   <>
+    <Container>
+      <img src={logoImg} alt="ezOrders" />
+      <Orders />
+    </Container>
     <GlobalStyle />
-    <h1>Hello World</h1>
   </>
 );
 
