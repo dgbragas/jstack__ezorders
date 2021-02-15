87ae9c54e7d0de4340c7497972910f2fb52b8641
dgbragas <dgbragas.dev@gmail.com>
2021-02-15 19:31:14 -0300

    feat: creating <App /> container styles

---
+++ web/src/styles/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin: 64px auto;
+  max-width: 740px;
+  padding: 0 24px;
+  width: 100%;
+`;
