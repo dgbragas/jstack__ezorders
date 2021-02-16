541a6939e2e58520efa6c40975f4733abed1882e
dgbragas <dgbragas.dev@gmail.com>
2021-02-15 21:12:32 -0300

    feat: project startup with expo-cli + linters

---
+++ app/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks", "prettier"],
+  rules: {
+    "import/prefer-default-export": "off",
+    "no-underscore-dangle": "off",
+    "no-use-before-define": "off",
+    "prettier/prettier": "error",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".js"],
+      },
+    ],
+  },
+};
