cb10cfebe978096bd987b22b1109914216286d0b
dgbragas <dgbragas.dev@gmail.com>
2021-02-15 23:32:12 -0300

    feat: adding styles to <Home />

---
+++ app/src/styles/index.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+  align-items: center;
+  flex: 1;
+`;
+
+export const Logo = styled.Image`
+  margin-top: 80px;
+`;
+
+export const NewOrderContainer = styled.View`
+  background: #fff;
+  bottom: 0;
+  box-shadow: 0px 0px 13px rgba(0, 0, 0, 0.25);
+  height: 88px;
+  justify-content: center;
+  padding: 0 24px;
+  position: absolute;
+  width: 100%;
+`;
